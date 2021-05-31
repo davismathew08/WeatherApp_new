d5b9b0506a516980b082c3648019cd4389bc3450
Daviz <davismathew08@gmail.com>
2021-05-31 12:01:27 +0530

    delete fonts

--- app/src/main/java/com/example/weatherappkotlin/ui/startup/splash/SplashActivity.kt
+++ app/src/main/java/com/example/weatherappkotlin/ui/startup/splash/SplashActivity.kt
@@ -1,13 +1,11 @@
 package com.example.weatherappkotlin.ui.startup.splash
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weatherappkotlin.R
-import com.example.weatherappkotlin.ui.main.MainActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 import java.util.*
 import kotlin.concurrent.timerTask
